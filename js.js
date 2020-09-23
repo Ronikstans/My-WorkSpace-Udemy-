f3486ab0294c8d2b507fb6aca935ebc946d6915d
Mikhail <Schkel@mail.ru>
2020-09-23 14:40:46 +0300

    Initial commit

---
+++ js.js
@@ -0,0 +1,65 @@
+
+// const category = 'toys';
+// console.log (`https//someurl.com/${category}/5`);
+
+
+const user = "ivan"; 
+alert(`привет, ${user}`);
+
+
+
+
+
+// alert ('hello');
+
+// const result = confirm ('Are you here?');
+// console.log (result);
+
+
+// const answer = +prompt ('Вам есть 18 лет?', '');
+// console.log ((answer + 5)); 
+
+
+const answers = [];
+console.log(typeof(answers));
+
+// answers[0] = prompt ('Как выше имя?', '');
+// answers[1] = prompt ('Сколько вам лет?', '');
+// answers[2] = prompt ('Как твои дела?', '');
+
+
+
+// let number = 5;
+// const leftBorderWidth = 5;
+
+// number = 10; 
+
+// console.log (number);
+
+
+// const obj = {
+//     a:5
+// };
+
+// obj.a = 40;
+ 
+// console.log (obj);
+
+
+// console.log(4/0);
+
+
+// let mish;
+// console.log(mish);
+
+
+// const obj = {
+//     name: 'jonh',
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.isMarried);
+
+// let arr = ['plum.png', 'orange.jpg', [], 7]; 
+// console.log(arr[2]);
---
+++ user.js
@@ -0,0 +1,28 @@
+// var buffer = new ArrayBuffer(16);
+
+// if (buffer.byteLength === 16) {
+//     console.log("Да, это 16 байт.");
+//   } else {
+//     console.log("О нет, размер не наш!");
+//   } 
+
+
+console.log ("arr" + " - object"); 
+
+let incr = 10,
+    decr = 10;
+
+// incr ++; 
+// decr --;
+
+console.log(++incr);
+console.log(++decr);
+
+
+console.log(14%5);
+
+
+const isCheked = true,
+      isClosed = false; 
+
+console.log (isCheked && !isClosed); 
