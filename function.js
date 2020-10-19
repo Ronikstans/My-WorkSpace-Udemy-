65d66092fd8767d14c25f53e9b9a6efb65406c73
MikhailHome <schkel@mail.ru>
2020-10-19 20:16:49 +0300

    hotfix

--- function.js
+++ function.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 "use strict";
 
 let num = 20; // глобальная переменная 
@@ -44,3 +45,39 @@ logger();
 const calcs = (a, b) => { return a + b } ;
 
 
+=======
+let a = 3;
+
+function addTwo(x) {
+  let ret = x + 2;
+  return ret;
+}
+let b = addTwo(a);
+console.log(b);
+
+
+
+let val1 = 2;
+
+function multiplyThis(n) {
+  let ret = n * val1;
+  return ret;
+}
+let multiplied = multiplyThis(6);
+console.log('example of scope:', multiplied);
+
+
+
+let val = 7;
+
+function createAdder() {
+  function addNumbers(a, b) {
+    let ret = a + b;
+    return ret;
+  }
+  return addNumbers;
+}
+let adder = createAdder();
+let sum = adder(val, 8);
+console.log('example of function returning a function: ', sum);
+>>>>>>> fb78cb038dad477a0d6142887055e1528492ccce
